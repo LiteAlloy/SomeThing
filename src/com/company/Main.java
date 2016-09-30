aa540453159ed9901f38a3e43381595fc2f87222
LiteAlloy <pornodoggy@mail.ru>
2016-09-30 23:52:15 +0300

    jframe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import javax.swing.*;
+
+import static javax.swing.JFrame.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("22 in pocket");
+
+        SimpleClass sc = new SimpleClass();
+        sc.setBounds(100,100,100,100);
+        sc.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        sc.setVisible(true);
     }
 }
---
+++ src/com/company/SimpleClass.java
@@ -0,0 +1,7 @@
+package com.company;
+
+
+import javax.swing.*;
+
+class SimpleClass extends JFrame{
+}
