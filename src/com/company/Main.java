426ca9f606b12a35a9327ead4c6da15437206a23
LiteAlloy <pornodoggy@mail.ru>
2016-10-04 14:13:09 +0300

    ignore some files 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,75 @@
 package com.company;
 
 import javax.swing.*;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.Duration;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Calendar;
 
 import static javax.swing.JFrame.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello");
         System.out.println("22 in pocket");
 
         SimpleClass sc = new SimpleClass();
+        SecondImplementClass sic = new SecondImplementClass();
+        sic.ThrowIt();
+        sic.getIt(new Duration() {
+            @Override
+            public int getSign() {
+                return 0;
+            }
+
+            @Override
+            public Number getField(DatatypeConstants.Field field) {
+                return null;
+            }
+
+            @Override
+            public boolean isSet(DatatypeConstants.Field field) {
+                return false;
+            }
+
+            @Override
+            public Duration add(Duration rhs) {
+                return null;
+            }
+
+            @Override
+            public void addTo(Calendar calendar) {
+
+            }
+
+            @Override
+            public Duration multiply(BigDecimal factor) {
+                return null;
+            }
+
+            @Override
+            public Duration negate() {
+                return null;
+            }
+
+            @Override
+            public Duration normalizeWith(Calendar startTimeInstant) {
+                return null;
+            }
+
+            @Override
+            public int compare(Duration duration) {
+                return 0;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+        });
         
         //11
 
